5e0a1067b91e7d2382fc28717f7536c5f2aee807
ivanchepurkin <ivanchepurkin18@gmail.com>
2025-08-03 11:43:21 +0300

    adjusted project to TS updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+
+    headless: false,
     trace: 'on-first-retry',
   },
 
@@ -38,16 +40,6 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
     },
 
     /* Test against mobile viewports. */
