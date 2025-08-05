a839655efb22e58dce937b157848e593557cfeff
irakly <irakly.filev.web@gmail.com>
2025-08-05 23:55:27 +0300

    Добавил правки по ревью

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,5 @@
-/*import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
+/*test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
@@ -22,17 +14,17 @@ import { test, expect } from '@playwright/test';
 
 test('Позитивный сценарий логина', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.fill('//input[@placeholder=\'Username\']', 'standard_user');
-  await page.fill('//input[@placeholder=\'Password\']', 'secret_sauce');
-  await page.click('//input[@type=\'submit\' and @class=\'submit-button btn_action\']'); //на всякий случай еще по классу определил
+  await page.fill('//input[@id=\'user-name\']', 'standard_user');
+  await page.fill('//input[@id=\'password\']', 'secret_sauce');
+  await page.click('//input[@id=\'login-button\']');
   await expect(page.locator('//span[text()=\'Products\']')).toHaveText('Products');
 });
 
 test('Негативный сценарий логина', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.fill('//input[@placeholder=\'Username\']', 'wrong_user');
-  await page.fill('//input[@placeholder=\'Password\'', 'wrong_pass');
-  await page.click('//input[@type=\'submit\' and @class=\'submit-button btn_action\']');
+  await page.fill('//input[@id=\'user-name\']', 'wrong_user');
+  await page.fill('//input[@id=\'password\']', 'wrong_pass');
+  await page.click('//input[@id=\'login-button\']');
   await expect(page.locator('//span[text()=\'Products\']')).toBeVisible();
 });
 
