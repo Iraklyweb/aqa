ef976136616267879488746352b889bc9a3d2a66
Iraklyweb <irakly.filev.web@gmail.com>
2025-08-16 12:26:03 +0300

    убрал комменты

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -19,13 +19,10 @@ test('Негативный сценарий логина', async ({ page }) => {
 
 test('Добавление товара в корзину', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
-    // логин
     await page.fill('#user-name', 'standard_user');
     await page.fill('#password', 'secret_sauce');
     await page.click('#login-button');
-    // добавить товар
     await page.click('#add-to-cart-sauce-labs-backpack');
-    // перейти в корзину
     await page.click('#shopping_cart_container');
     await expect(page.locator('[data-test="inventory-item"]')).toBeVisible();
 });
@@ -35,29 +32,23 @@ test('Оформление заказа (checkout flow)', async ({ page }) => {
     await page.fill('#user-name', 'standard_user');
     await page.fill('#password', 'secret_sauce');
     await page.click('#login-button');
-    // добавить товар
     await page.click('#add-to-cart-sauce-labs-backpack');
-    // перейти в корзину
     await page.click('#shopping_cart_container');
-    // судя по тз нам не обязательно видеть карточку await expect(page.locator('.cart_list')).toBeVisible();
     await page.click('#checkout');
-    // заполнить форму
     await page.fill('#first-name', 'TestFirstName');
     await page.fill('#last-name', 'TestLastName');
     await page.fill('#postal-code', '30301');
-    await page.click('#continue'); // Continue
-    await page.click('#finish'); // Finish
+    await page.click('#continue');
+    await page.click('#finish');
     await expect(page.locator('[data-test="complete-header"]')).toHaveText('Thank you for your order!');
 });
 
 test('Проверка фильтрации товаров', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
-    // логин
     await page.fill('#user-name', 'standard_user');
     await page.fill('#password', 'secret_sauce');
     await page.click('#login-button');
     await page.click('.select_container');
-    // выбрать сортировку
     await page.selectOption('[data-test="product-sort-container"]', 'lohi');
     // проверить порядок цен/in progress
 });
